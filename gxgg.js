abec85c8904879d0573979c45806bb2ef3d4ef95
hotmusicweb <126324587+hotmusicweb@users.noreply.github.com>
2023-03-05 18:56:06 +0800

    Add files via upload

---
+++ gxgg.js
@@ -0,0 +1,7 @@
+
+document.getElementById("gx").innerHTML = `
+<div id="gxts" style="display:none;">
+   <div style="position: fixed;bottom: 0;width:100%;transform:translate(0px,-117px);">
+     <div style="width:110px;height:42px;border-radius:14px;border:1px solid;margin:0 auto;background-color:#ffffff;color:#E0E0E0;box-shadow: 0px 1px 49px rgba(0, 0, 0, 0.1); "><p style="color:#000;text-align:center;font-size:12px;transform:translate(0px,1.6px);">检查到最新版本</p></div>
+   </div>
+</div>`;
